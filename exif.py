5369d09c46f88c899ed2b9bfe953c3ab6905fa11
Anton Golubtsov <augolubtsov@95.108.172.192-red.dhcp.yndx.net>
2014-01-09 14:35:38 +0400

    Add extract_exif func

--- exif.py
+++ exif.py
@@ -1,16 +1,21 @@
+#!/usr/bin/env python
+
 import Quartz
 import CoreFoundation as CF
 import os, sys
 
-def main(args):
-    filename = os.path.abspath(sys.argv[1]) 
+def extract_exif(filename):
     url = CF.CFURLCreateFromFileSystemRepresentation(None, filename, len(filename), False)
   
     img_src = Quartz.ImageIO.CGImageSourceCreateWithURL(url, {})
     properties = Quartz.ImageIO.CGImageSourceCopyPropertiesAtIndex(img_src, 0, None)
-    exif = properties[Quartz.ImageIO.kCGImagePropertyExifDictionary]
+    return properties[Quartz.ImageIO.kCGImagePropertyExifDictionary]
+
+def main(args):
+    filename = os.path.abspath(args[1])
+    print extract_exif(filename)[Quartz.ImageIO.kCGImagePropertyExifDateTimeOriginal]
    
-    img_dest = Quartz.ImageIO.CGImageDestinationCreateWithURL(url, 'public.jpeg', 1, None)
+    #img_dest = Quartz.ImageIO.CGImageDestinationCreateWithURL(url, 'public.jpeg', 1, None)
     
     #exif[Quartz.ImageIO.kCGImagePropertyExifDateTimeOriginal] = u'2009:06:17 21:03:18'
      
