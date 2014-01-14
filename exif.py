9ca7f831facbbc624e4e68cc4118aed25ed15108
Anton Golubtsov <augolubtsov@95.108.172.192-red.dhcp.yndx.net>
2014-01-14 18:47:35 +0400

    Add command line args and logging
    usage: exif.py [-h] [-d] [-f] [-v] [filename]
    
    positional arguments:
      filename          name of file with photo, if empty then use stdin
    
    optional arguments:
      -h, --help        show this help message and exit
      -d, --outdir      print converted name of input file in format
                        outdir/DateTime.jpg - DateTime from --dateformat
    option
      -f, --dateformat  format for --outdir option (default="%Y-%m/%Y-%m-%d
                        %H-%M-%S"
      -v, --verbose     increase output verbosity

--- exif.py
+++ exif.py
@@ -1,33 +1,65 @@
 #!/usr/bin/env python
 
+import argparse
+import logging
 import Quartz
 import CoreFoundation as CF
 import os, sys
 
-def extract_exif(filename):
-    url = CF.CFURLCreateFromFileSystemRepresentation(None, filename, len(filename), False)
-  
-    img_src = Quartz.ImageIO.CGImageSourceCreateWithURL(url, {})
-    properties = Quartz.ImageIO.CGImageSourceCopyPropertiesAtIndex(img_src, 0, None)
-    return properties[Quartz.ImageIO.kCGImagePropertyExifDictionary]
+class ExifProcessor(object):
+    """docstring for ExifProcessor"""
+    def __init__(self, verbose=0, outdir=None, dateformat='%%Y-%%m/%%Y-%%m-%%d %%H-%%M-%%S'):
+        super(ExifProcessor, self).__init__()
+        self.verbose = verbose
+        self.outdir = outdir
+        self.dateformat = dateformat
+        
+    def extract_exif(self, filename):
+        logging.debug('extract_exif(filename="%s"', filename)
+        url = CF.CFURLCreateFromFileSystemRepresentation(None, filename, len(filename), False)      
+        img_src = Quartz.ImageIO.CGImageSourceCreateWithURL(url, {})
+        properties = Quartz.ImageIO.CGImageSourceCopyPropertiesAtIndex(img_src, 0, None)
+        result = properties[Quartz.ImageIO.kCGImagePropertyExifDictionary]
+        if self.verbose > 1:
+            logging.debug('exif_dump\n%s', result)
+        return result
 
-def process_file(filename):
-    absfilename = os.path.abspath(filename)
-    exif = extract_exif(absfilename)
-    print "%s\t%s" %(filename,exif[Quartz.ImageIO.kCGImagePropertyExifDateTimeOriginal])
+    def process_file(self, filename):
+        logging.debug('process_file(filename="%s"', filename)
+        absfilename = os.path.abspath(filename)
+        exif = self.extract_exif(absfilename)
+        date_from_exif = exif[Quartz.ImageIO.kCGImagePropertyExifDateTimeOriginal]
+        logging.debug('process_file date_from_exif="%s"', date_from_exif)
+        print "%s\t%s" %(filename, date_from_exif)
+
+    def process_files(self, files):
+        logging.debug('process_files()')
+        for v in files:
+            v = v[:-1]
+            if v:
+                self.process_file(v)
 
 def main(args):
-    filename = args[1]
-    process_file(filename)
-   
-    #img_dest = Quartz.ImageIO.CGImageDestinationCreateWithURL(url, 'public.jpeg', 1, None)
-    
-    #exif[Quartz.ImageIO.kCGImagePropertyExifDateTimeOriginal] = u'2009:06:17 21:03:18'
-     
-    #Quartz.ImageIO.CGImageDestinationAddImageFromSource(\
-    #           img_dest, img_src, 0, {Quartz.ImageIO.kCGImagePropertyExifDictionary: exif})
-      
-    #Quartz.ImageIO.CGImageDestinationFinalize(img_dest)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", nargs='?',
+                    help="name of file with photo, if empty then use stdin")
+    parser.add_argument("-d", "--outdir", action="count",
+                    help='print converted name of input file in format outdir/DateTime.jpg - DateTime from --dateformat option')
+    parser.add_argument("-f", "--dateformat", action="store_true",
+                    help='format for --outdir option (default="%%Y-%%m/%%Y-%%m-%%d %%H-%%M-%%S"', default='%%Y-%%m/%%Y-%%m-%%d %%H-%%M-%%S')    
+    parser.add_argument("-v", "--verbose", action="count",
+                    help="increase output verbosity")
+    args = parser.parse_args()
+    if args.verbose:
+        logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+    else:
+        logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
+    ep = ExifProcessor(args.verbose, args.outdir, args.dateformat)
+    if args.filename:
+        ep.process_file(args.filename)
+    else:
+        ep.process_files(sys.stdin)
+   
 if __name__ == '__main__':
     main(sys.argv)
